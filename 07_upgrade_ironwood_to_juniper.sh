e16a6bb2c24b5617ab7cbc6eea03529894067242
Amir Tadrisi <amirtds@gmail.com>
2025-01-14 17:07:09 -0500

    chore: change fake migrations

--- 07_upgrade_ironwood_to_juniper.sh
+++ 07_upgrade_ironwood_to_juniper.sh
@@ -153,26 +153,15 @@ pv -s $(stat --format=%s "$EXPORT_DIR/openedx.sql") "$EXPORT_DIR/openedx.sql" | 
 
 # Run fake migrations for problematic apps
 echo "Running fake migrations for specific apps..."
-# TO prevent migration error for 0001 lets run on the next dry run.
-tutor local run lms sh -c "python manage.py lms migrate content_type_gating zero --fake"
-tutor local run lms sh -c "python manage.py lms migrate content_type_gating --fake-initial"
-tutor local run lms sh -c "python manage.py lms migrate content_type_gating"
 
-tutor local run lms sh -c "python manage.py lms migrate content_type_gating 0001 --fake"
 tutor local run lms sh -c "python manage.py lms migrate content_type_gating 0006 --fake"
 tutor local run lms sh -c "python manage.py lms migrate content_type_gating 0007 --fake"
 tutor local run lms sh -c "python manage.py lms migrate course_date_signals 0001 --fake"
-tutor local run lms sh -c "python manage.py lms migrate course_duration_limits 0001 --fake"
-tutor local run lms sh -c "python manage.py lms migrate course_duration_limits 0003 --fake"
 tutor local run lms sh -c "python manage.py lms migrate course_duration_limits 0006 --fake"
 tutor local run lms sh -c "python manage.py lms migrate course_duration_limits 0007 --fake"
-
 tutor local run lms sh -c "python manage.py lms migrate discounts 0001 --fake"
 tutor local run lms sh -c "python manage.py lms migrate discounts 0002 --fake"
 tutor local run lms sh -c "python manage.py lms migrate thumbnail 0001 --fake"
-tutor local run lms sh -c "python manage.py lms migrate track 0002 --fake"
-tutor local run lms sh -c "python manage.py lms migrate user_authn 0001 --fake"
-
 
 
 # Run remaining migrations
